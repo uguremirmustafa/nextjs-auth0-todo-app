bea9f1f0e3faf6b686cc5853609d3e2386c90608
uguremirmustafa <uguremirmustafa@gmail.com>
2020-12-12 12:10:26 +0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+/** @format */
+
+import '../styles/index.css';
+import { TodosProvider } from '../contexts/TodosContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <TodosProvider>
+      <div className="container mx-auto my-10 max-w-xl">
+        <Component {...pageProps} />
+      </div>
+    </TodosProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,56 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+/** @format */
 
-export default function Home() {
+import Head from 'next/head';
+import Navbar from '../components/Navbar';
+import Todo from '../components/Todo';
+import styles from '../styles/Home.module.css';
+import { table, minifyRecords } from './api/utils/Airtable';
+import { TodosContext } from '../contexts/TodosContext';
+import { useEffect, useContext } from 'react';
+import auth0 from './api/utils/auth0';
+import TodoForm from '../components/TodoForm';
+
+export default function Home({ initialTodos, user }) {
+  const { todos, setTodos } = useContext(TodosContext);
+  useEffect(() => {
+    setTodos(initialTodos);
+  }, []);
+  console.log(user);
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Todo Authenticated App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <Navbar user={user} />
+      <main>
+        {user && (
+          <>
+            <TodoForm />
+            <ul>{todos && todos.map((todo) => <Todo key={todo.id} todo={todo} />)}</ul>
+          </>
+        )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await auth0.getSession(context.req);
+  try {
+    const todos = await table.select({}).firstPage();
+    return {
+      props: {
+        initialTodos: minifyRecords(todos),
+        user: session?.user || null,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      props: {
+        error: 'something went wrong',
+      },
+    };
+  }
 }
