940ec48f6c08465d5f529485bdc288dabecca29f
uguremirmustafa <uguremirmustafa@gmail.com>
2020-12-13 21:23:19 +0300

    debuggin postcss.config

--- pages/api/utils/auth0.js
+++ pages/api/utils/auth0.js
@@ -7,8 +7,8 @@ export default initAuth0({
   clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_SECRET,
   scope: 'openid profile',
-  redirectUri: 'http://localhost:3000/api/callback',
-  postLogoutRedirectUri: 'http://localhost:3000/',
+  redirectUri: 'https://wehavesometodo.vercel.app/api/callback',
+  postLogoutRedirectUri: 'https://wehavesometodo.vercel.app/',
   session: {
     // The secret used to encrypt the cookie.
     cookieSecret: process.env.COOKIE_SECRET,
